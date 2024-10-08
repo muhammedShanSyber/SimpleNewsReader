cf26a0f8a33c46b1fa8156cb13943bdaf6e62922
Mohammed Shan <suharabeevi443@outlook.com>
2024-10-08 07:43:52 -0400

    created some basic UI components and installed necessary libraries and packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+import axios from 'axios'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const newsApi = ""
+  useEffect(() => {
+    fetchNews();
+  })
+
+  const fetchNews = async () => {
+    // console.log('This text is working');
+    console.log(newsApi)
+    const Data = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=0f5722122ab7419ab5f71db0233c7fda`)
+    const NewsComponents = Data.data.articles;
+    console.log(NewsComponents)
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Container>
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand>Simple News Reader</Navbar.Brand>
+          </Container>
+        </Navbar>
+      </Container>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
     </>
   )
 }
